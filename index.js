7b64b7303e834e403414153cd1a871d98a2c43f9
bhaskar359 <bhaskarnarendra359@gmail.com>
2024-08-12 12:59:41 +0530

    solution added

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -21,10 +21,27 @@ app.get("/", function (req, res) {
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: 'hello API' });
 });
 
-
+app.get('/api/:date?', (req, res) => {
+  let date;
+  if (!req.params.date) {
+    date = new Date();
+  }
+  else {
+    if (!isNaN(req.params.date)) {
+      date = new Date(parseInt(req.params.date));
+    } else {
+      date = new Date(req.params.date);
+    }
+  }
+  if (isNaN(date.getTime())) {
+    res.json({ error: 'Invalid Date' });
+  } else {
+    res.json({ unix: date.getTime(), utc: date.toUTCString() });
+  }
+})
 
 // Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
